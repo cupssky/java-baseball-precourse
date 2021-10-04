e4062d7e375ee96c7e2726ca3c4aa15d5faeca45
cupssky <cupssky@naver.com>
2021-10-04 17:33:11 +0900

    feat: 게임결과가 스트라이크3 나오기 전까지 반복진행 구현 - Game.java 변경 / 스트라이크3이 나오면 종료됨

--- src/main/java/baseball/core/Game.java
+++ src/main/java/baseball/core/Game.java
@@ -1,14 +1,24 @@
 package baseball.core;
 
+import java.util.Set;
+
 public class Game {
 
+  private Referee referee;
   private Hitter hitter;
+  private Pitcher pitcher;
 
   public Game() {
+    referee = new Referee();
     hitter = new Hitter();
+    pitcher = new Pitcher();
   }
 
   public void start() {
-    hitter.swing();
+    Set<Integer> swing = hitter.swing();
+    boolean end;
+    do {
+      end = referee.judge(swing, pitcher.throwBall());
+    } while (!end);
   }
 }
