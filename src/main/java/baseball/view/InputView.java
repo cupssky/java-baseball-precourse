d478c51c2a54e1e197a6326f9c71ea4fac149e78
DAMICHOI <choidami93@gmail.com>
2021-10-06 04:15:15 +0900

    feat : 재시작 질문에 대한 답변 입력 받는 기능 추가
    재시작 질문에 대한 답변 입력 받아오는 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,14 +5,21 @@ import baseball.config.GameMessage;
 
 public class InputView {
 
-    public static final String INPUT_MESSAGE = GameMessage.INPUT_MESSAGE;
-
     /**
-     * 입력값 받아오기
+     * Player의 랜덤 숫자 입력값 받아오기
      * @return String
      */
     public String inputValue() {
-        System.out.print(INPUT_MESSAGE);
+        System.out.print(GameMessage.INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    /**
+     * 재시작 질문에 대한 답변 입력값 받아오기
+     * @return String
+     */
+    public String inputRestartQuestionAsk() {
+        System.out.println(GameMessage.GAME_RESTART_QUESTION_MESSAGE);
         return Console.readLine();
     }
 }
