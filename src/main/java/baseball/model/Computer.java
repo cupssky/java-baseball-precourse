62a3e069365625695adc4674e3772f467f9213c8
DAMICHOI <choidami93@gmail.com>
2021-10-06 04:11:05 +0900

    feat : 야구 게임 점수를 구하는 기능 추가
    - 스트라이크, 볼, 낫싱을 판단하는 기능 추가
    - 힌트를 생성하는 기능 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -3,6 +3,8 @@ package baseball.model;
 import baseball.config.BallConfig;
 import java.util.ArrayList;
 import java.util.List;
+
+import baseball.config.GameMessage;
 import nextstep.utils.Randoms;
 
 public class Computer {
@@ -26,7 +28,7 @@ public class Computer {
      */
     public void setRandomNumbers() {
         int randomNumber;
-        while (this.randomNumbers.size() != this.INPUT_SIZE) {
+        while (randomNumbers.size() != INPUT_SIZE) {
             randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             addRandomNumbers(randomNumber);
         }
@@ -38,7 +40,7 @@ public class Computer {
      */
     public void addRandomNumbers(int randomNumber) {
         if (!isDuplication(randomNumber)) {
-            this.randomNumbers.add(randomNumber);
+            randomNumbers.add(randomNumber);
         }
     }
 
@@ -58,6 +60,88 @@ public class Computer {
      * @return boolean
      */
     public boolean isDuplication(int randomNumber) {
-        return this.randomNumbers.contains(randomNumber);
+        return randomNumbers.contains(randomNumber);
+    }
+
+    /**
+     * 힌트를 생성한다.
+     * @return String
+     */
+    public String makeHint(List<Integer> computerNumberList, List<Integer> playerNumberList) {
+        String hint = "";
+        int strikeScore = strikeScore(computerNumberList, playerNumberList);
+        int ballScore = ballScore(computerNumberList, playerNumberList);
+
+        if (strikeScore > 0) {
+            hint += strikeScore + GameMessage.STRIKE;
+        }
+
+        if (ballScore > 0) {
+            hint += ballScore + GameMessage.BALL;
+        }
+
+        if (strikeScore == 0 && ballScore == 0) {
+            hint += GameMessage.NOTHING;
+        }
+
+        return hint.trim();
+    }
+
+    /**
+     * Strike 점수를 구한다. <br/>
+     * 같은 위치에 같은 수가 있으면 스트라이크
+     * @return int
+     */
+    public int strikeScore(List<Integer> computerNumberList, List<Integer> playerNumberList) {
+        int strikeScore = 0;
+
+        for (int i=0; i<INPUT_SIZE; i++) {
+            strikeScore += isStrike(computerNumberList.get(i), playerNumberList.get(i));
+        }
+        return strikeScore;
+    }
+
+    /**
+     * Ball 점수를 구한다. <br/>
+     * 다른 자리에 포함하는 숫자가 있으면 볼
+     * @return int
+     */
+    public int ballScore(List<Integer> computerNumberList, List<Integer> playerNumberList) {
+        int ballScore = 0;
+
+        for (int i=0; i<INPUT_SIZE; i++) {
+            ballScore += isBall(i, computerNumberList, playerNumberList);
+        }
+        return ballScore;
+    }
+
+    /**
+     * Strike 여부를 확인한다. <br/>
+     * 같은 위치에 같은 숫자가 있을 경우 1을 반환한다.
+     * @return int
+     */
+    private int isStrike(int computerNumber, int playerNumber) {
+        int score = 0;
+
+        if (computerNumber == playerNumber) {
+            score = 1;
+        }
+        return score;
+    }
+
+    /**
+     * Ball 여부를 확인한다. <br/>
+     * 같은 위치가 아니면서, 선택한 숫자가 포함되어 있을 경우 1을 반환한다.
+     * @return int
+     */
+    private int isBall(int index, List<Integer> computerNumberList, List<Integer> playerNumberList) {
+        int score = 0;
+        int computerNumber = computerNumberList.get(index);
+        int playerNumber = playerNumberList.get(index);
+
+        if (computerNumber != playerNumber && computerNumberList.contains(playerNumber)) {
+            score = 1;
+        }
+        return score;
     }
 }
