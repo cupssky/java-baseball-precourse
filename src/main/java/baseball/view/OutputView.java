e2e6410b93c9adcf57b8705a66e27c951cb8cdd4
DAMICHOI <choidami93@gmail.com>
2021-10-06 04:05:56 +0900

    feat : 출력을 위한 view 추가
    힌트 내용 및 게임 종료 메시지 출력을 위한 View 추가

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,26 @@
+package baseball.view;
+
+import baseball.config.GameMessage;
+import baseball.model.Computer;
+
+public class OutputView {
+    private final Computer computer;
+
+    public OutputView() {
+        this.computer = new Computer();
+    }
+
+    /**
+     * 힌트 내용을 출력한다.
+     */
+    public void outputScore(String hint) {
+        System.out.println(hint);
+    }
+
+    /**
+     * 게임 종료 메시지를 출력한다.
+     */
+    public void quitMessage() {
+        System.out.println(GameMessage.GAME_END_MESSAGE);
+    }
+}
