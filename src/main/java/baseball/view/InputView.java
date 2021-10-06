6e60118865294225a4902ddd2a6eadfbc88e7459
DAMICHOI <choidami93@gmail.com>
2021-10-06 21:35:46 +0900

    feat : 플레이어의 입력값에 대한 유효성 검증 및 예외 처리 로직 추가
    - 플레이어의 입력값에 대한 유효성 검증 로직 추가
    - 유효성 검증 시 예외 처리 로직 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,17 +1,25 @@
 package baseball.view;
 
+import baseball.config.BallConfig;
+import baseball.exception.GameException;
 import nextstep.utils.Console;
 import baseball.config.GameMessage;
 
-public class InputView {
+public class InputView extends GameException {
 
     /**
-     * Player의 랜덤 숫자 입력값 받아오기
+     * Player의 랜덤 숫자 입력값 받아오기 <br/>
+     * 오류 발생 시 Error 메시지 출력 후 계속 진행한다.
      * @return String
      */
     public String inputValue() {
-        System.out.print(GameMessage.INPUT_MESSAGE);
-        return Console.readLine();
+        String inputNumber;
+        do {
+            System.out.print(GameMessage.INPUT_MESSAGE);
+            inputNumber = Console.readLine();
+        } while (!checkInputValueValication(inputNumber));
+
+        return inputNumber;
     }
 
     /**
@@ -21,5 +29,61 @@ public class InputView {
     public String inputRestartQuestionAsk() {
         System.out.println(GameMessage.GAME_RESTART_QUESTION_MESSAGE);
         return Console.readLine();
+    }
+
+    /**
+     * 입력값에 대한 유효성 검사
+     * @return boolean
+     */
+    public boolean checkInputValueValication(String inputValue) {
+        try {
+            checkNumberLength(inputValue);
+            checkOnlyNumber(inputValue);
+            checkNumberScope(inputValue);
+        } catch (Exception e) {
+            System.err.println("checkInputValueValication() Exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 입력값이 숫자가 아니면 오류를 반환한다.
+     * @param inputValue 입력값
+     */
+    private void checkOnlyNumber(String inputValue) {
+        if (!inputValue.matches("[0-9]+")) {
+            throw new GameException(GameMessage.ERROR_MESSAGE_INPUT_ONLY_NUMBER);
+        }
+    }
+
+    /**
+     * 입력값이 갯수가 INPUT_SIZE와 다르면 오류를 반환한다.
+     * @param inputValue 입력값
+     */
+    private void checkNumberLength(String inputValue) {
+        if (inputValue.length() != BallConfig.INPUT_SIZE) {
+            throw new GameException(GameMessage.ERROR_MESSAGE_INPUT_NUMBER_LENGTH);
+        }
+    }
+
+    /**
+     * 입력값 숫자의 범위를 체크한다.
+     * @param inputValue 입력값
+     */
+    private void checkNumberScope(String inputValue) {
+        for (int i=0; i<inputValue.length(); i++) {
+            isNumberInScope(inputValue.charAt(i));
+        }
+    }
+
+    /**
+     * 입력값이 정해진 범위에서 벗어나면 오류를 반환한다.
+     * @param charAt 특정 위치의 입력값
+     */
+    private void isNumberInScope(char charAt) {
+        if (charAt - '0' < BallConfig.MIN_NUMBER || charAt - '0' > BallConfig.MAX_NUMBER) {
+            throw new GameException(GameMessage.ERROR_MESSAGE_INPUT_NUMBER_SCOPE);
+        }
     }
 }
